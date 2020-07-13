623140345496532e85ab4ddd829ded5b2f6cd155
burndaflame <6634023+burndaflame@users.noreply.github.com>
2020-07-13 19:35:30 +0200

    Update greasemonkey_autoload.user.js

--- greasemonkey_autoload.user.js
+++ greasemonkey_autoload.user.js
@@ -13,29 +13,26 @@
     var observerOptions = {
         childList: true,
         subtree: true
-    }
+    };
 
     var observer = new MutationObserver(function(mutationList, observer) {
         mutationList.forEach((mutation) => {
             if (Array.from(mutation.removedNodes).some((element) => {
-                return element.className === "loadingScreen";
-            })) {
+                    return element.className === "loadingScreen";
+                })) {
                 observer.disconnect();
-                var xhr=new XMLHttpRequest;
-                var urls = ["https://raw.githubusercontent.com/douglascrockford/JSON-js/master/cycle.js",
-                            "https://raw.githubusercontent.com/burndaflame/stlsaver/master/herosaver.js"];
-                urls.forEach(function(url, index){
-                    xhr.open("get",url,true);
-                    xhr.onreadystatechange=function(){
-                        if (xhr.readyState == 4) {
-                            var script=document.createElement("script");
-                            script.type="text/javascript";
-                            script.text=xhr.responseText;
-                            document.body.appendChild(script)
-                        }
-                    };
-                    xhr.send(null);
-                }
+                var xhr = new XMLHttpRequest;
+
+                xhr.open("get", "https://raw.githubusercontent.com/burndaflame/stlsaver/master/herosaver.js", true);
+                xhr.onreadystatechange = function() {
+                    if (xhr.readyState == 4) {
+                        var script = document.createElement("script");
+                        script.type = "text/javascript";
+                        script.text = xhr.responseText;
+                        document.body.appendChild(script);
+                    }
+                };
+                xhr.send(null);
             }
         });
     });
